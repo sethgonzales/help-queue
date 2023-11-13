3c10d012ba49f79d5d441587c1c1dcb12ff47dd6
sethgonzales <sethgonzales157@gmail.com>
2023-11-13 11:47:09 -0800

    more stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import download from "./../img/download.jpg";
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <img src={download} alt="An image of tickets" />
+    </React.Fragment>
   );
 }
 
